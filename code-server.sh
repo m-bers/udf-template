6f2ffe0cbd7f9165031d4466e799d4b4866f2f05
m-bers <jc4gavejc@gmail.com>
2022-07-24 23:52:09 -0700

    p10k

--- code-server.sh
+++ code-server.sh
@@ -101,6 +101,6 @@ font-style: normal;
 EOF
 
 sudo systemctl restart nginx
-
+wget https://raw.githubusercontent.com/m-bers/udf-template/main/.p10k.zsh -P ~
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
