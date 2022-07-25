c421a71453457ffcd7933ab81b46a18c2ad9c020
m-bers <jc4gavejc@gmail.com>
2022-07-24 22:00:09 -0700

    add files

--- aws-creds.sh
+++ aws-creds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Set up AWS credentials in UDF
-mkdir ~/.aws
+mkdir -p ~/.aws
 printf "[default]\naws_access_key_id=" > ~/.aws/credentials
 curl -s 10.1.1.1/cloudAccounts | jq '.cloudAccounts[0]' | jq -r ' .apiKey' >> ~/.aws/credentials
 printf "aws_secret_access_key=" >> ~/.aws/credentials
