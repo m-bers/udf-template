674dcfd48364f395e1501f95d60360ae45da40f0
m-bers <jc4gavejc@gmail.com>
2022-07-24 22:05:29 -0700

    quotes?

--- code-server.sh
+++ code-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export VERSION=4.5.1
-curl -fOL https://github.com/coder/code-server/releases/download/v$VERSION/code-server_$VERSION_amd64.deb
-sudo dpkg -i code-server_$VERSION_amd64.deb
+curl -fOL "https://github.com/coder/code-server/releases/download/v$VERSION/code-server_$VERSION_amd64.deb"
+sudo dpkg -i "code-server_$VERSION_amd64.deb"
 sudo systemctl enable --now code-server@$USER
