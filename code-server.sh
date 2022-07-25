bcdb40d44cb4931fe74b3a322c9e229ebeb6e592
m-bers <jc4gavejc@gmail.com>
2022-07-24 22:03:00 -0700

    code-server.sh

--- code-server.sh
+++ code-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=4.5.1
+export VERSION=4.5.1
 curl -fOL https://github.com/coder/code-server/releases/download/v$VERSION/code-server_$VERSION_amd64.deb
 sudo dpkg -i code-server_$VERSION_amd64.deb
 sudo systemctl enable --now code-server@$USER
